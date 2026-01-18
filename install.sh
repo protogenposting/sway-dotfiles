f4421e7bc47f09b7b7d21623da30876601d24187
protogenposting <86360309+protogenposting@users.noreply.github.com>
2026-01-18 12:58:26 -0500

    waybar

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 echo "installing stuff!!!"
-sudo pacman -S sddm sway xdg-desktop-portal mpv dolphin foot fuzzel fish discord slurp wl-clipboard grim swaybg swaync swaybar
+sudo pacman -S sway xdg-desktop-portal mpv dolphin foot fuzzel fish discord slurp wl-clipboard grim swaybg swaync
 yay -S linux-discord-rich-presence obs-cmd
